54b228a4e6c2d630988d8fde1f333975be62152e
viplab <ivankai.wck@gmail.com>
2018-06-12 15:39:31 +0800

    map加入tableview來Category選擇地圖顯示

---
+++ motdParser/地圖/mapTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  mapTableViewCell.swift
+//  
+//
+//  Created by viplab on 2018/6/12.
+//
+
+import UIKit
+
+class mapTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
