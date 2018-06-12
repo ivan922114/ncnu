2dc0d0f5ba55e25ab09cb5773a38a52faa9dd287
viplab <senglam199605@gmail.com>
2018-06-13 01:08:57 +0800

    更新課程查詢系統的版面配置

---
+++ motdParser/地圖/placesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  placesTableViewCell.swift
+//  motdParser
+//
+//  Created by viplab on 2018/6/13.
+//  Copyright © 2018年 Seng Lam. All rights reserved.
+//
+
+import UIKit
+
+class placesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
